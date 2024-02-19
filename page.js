99763754fbb2463cb724b20497be1ab4a8efb7a7
Cuchulahn O'Byrne <99617276+kushobyrne@users.noreply.github.com>
2024-02-19 15:44:52 +0000

    Add files via upload

---
+++ page.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	//$("#homepage").animate({left: "25vw", opacity: '.1'});
+	//alert("hello");
+	$("#takemeback").click(function(){
+		$(document).scrollTop(0);
+	});
+});
+/*$(window).scroll(function() {
+	if($(document).scrollTop() == 0)
+	{
+				
+	}
+	if($(document).scrollTop() > (750))
+	{
+				//$("#gifs").animate({left: "45vw", opacity: '.1'});
+	}
+	if($(document).scrollTop() > (750*2))
+	{
+				//$("#brands").animate({left: "25vw", opacity: '.1'});
+	}
+});*/
